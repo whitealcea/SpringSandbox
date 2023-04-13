f97eab5878a7ec2a2542bc6bfc72620a94124605
shiratori <y.shiratori000@gmail.com>
2023-04-13 22:47:35 +0900

    add_entities テーブル定義をもとにEntityクラスを生成

---
+++ src/main/java/com/example/springsandbox/entity/Attendance.java
@@ -0,0 +1,45 @@
+package com.example.springsandbox.entity;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * 勤怠 Entityクラス
+ */
+@Data
+public class Attendance {
+    /**
+     * 勤怠ID
+     */
+    private Integer id;
+    /**
+     * ユーザーID
+     */
+    private Integer userId;
+    /**
+     * 日付
+     */
+    private LocalDate date;
+    /**
+     * 出勤時間
+     */
+    private LocalTime startTime;
+    /**
+     * 退勤時間
+     */
+    private LocalTime endTime;
+    /**
+     * 休憩時間
+     */
+    private LocalTime breakTime;
+    /**
+     * 遅刻時間
+     */
+    private LocalTime lateTime;
+    /**
+     * 早退時間
+     */
+    private LocalTime leaveEarlyTime;
+}
